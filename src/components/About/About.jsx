9e7e7493dcc4d92805f4c17ef806bc6b3d66e0c5
Rituparna Pradhan <rituppradhan2004@gmail.com>
2025-09-23 20:32:56 +0530

    Added AboutMe SEction

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
 import './About.css';
+import theme_pattern from '../../assets/theme_pattern.svg'
+import profile_image from '../../assets/about_profile.svg'
 const About = () => {
   return (
-    <div>
-      
+    <div className='about'>
+      <div className="about-title">
+        <h1>About Me</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="about-section">
+        <div className="aboutleft">
+            <img src={profile_image} alt="" />
+        </div>
+        <div className="about-right">
+            <div className="about-para">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus porro voluptates necessitatibus eligendi earum, voluptas vitae sapiente corporis repellendus nobis!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, ex!</p>
+            </div>
+            <div className="about-skills">
+                <div className="about-skill">
+                    <p>HTML & CSS</p>
+                    <hr style={{width:"50%"}} />
+                </div>
+                <div className="about-skill">
+                    <p>HTML & CSS</p>
+                    <hr style={{width:"50%"}} />
+                </div>
+                <div className="about-skill">
+                    <p>HTML & CSS</p>
+                    <hr style={{width:"50%"}} />
+                </div>
+            </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+            <h1>10+</h1>
+            <p>Years of Experience</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+            <h1>10+</h1>
+            <p>Years of Experience</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+            <h1>10+</h1>
+            <p>Years of Experience</p>
+        </div>
+      </div>
     </div>
   )
 }
